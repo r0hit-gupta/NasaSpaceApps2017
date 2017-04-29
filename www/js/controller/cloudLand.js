7b267a4c28f919efb87d2427315ece18036a1006
r0hit-gupta <rohit1997gupta@gmail.com>
2017-04-29 23:23:32 +0530

    api changes

--- www/js/controller/cloudLand.js
+++
@@ -1,5 +0,0 @@
-app.controller('CloudLandCtrl', function($scope, $window) {
-    if ($window.ga) {
-        $window.ga.trackView('What is that?');
-    }
-});
--- www/js/filter/renderFilter.js
+++
@@ -1,9 +0,0 @@
-app.filter('CoordsFilter', function(){
-	return function (item) {
-		
-		feature = new ol.Feature(new ol.geom.Point(ol.proj.transform([item.lon, item.lat], Config.CRS, Config.mapProjection)));
-        feature.setProperties(item);
-        
-        return layer;
-    }
-});
